d8d59f69440d5d6cf930d6d4fe230b4adbb52760
themeetpatell <the.meetpatell@gmail.com>
2025-09-07 13:37:32 +0400

    Fix deployment configuration for startupos-web repository

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/StartupOS/',
+  base: '/startupos-web/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
